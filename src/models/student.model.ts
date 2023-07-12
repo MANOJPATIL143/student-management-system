608d7f7a83a0e9f5266f452f179e08eff271b896
elijah.ndungu <elijahndungu30@gmail.com>
2023-07-12 22:53:04 +0300

    chore: adding validations to sessions with zod

---
+++ src/models/student.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import { customAlphabet } from "nanoid";
+import { UserDocument } from "./user.model";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 10);
+
+export interface StudentInput {
+  user: UserDocument["_id"];
+  firstName: string;
+  lastName: string;
+  class: string;
+  admissionNo: string;
+  grade: string;
+  age: string;
+  guardianName: string;
+  guardianPhoneNumber: string;
+}
+
+export interface StudentDocument extends StudentInput, mongoose.Document {}
+
+const studentSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => `student_${nanoid()}`,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    class: { type: String, required: true },
+    admissionNo: { type: String, required: true },
+    grade: { type: String, required: true },
+    age: { type: String, required: true },
+    guardianName: { type: String, required: true },
+    guardianPhoneNumber: { type: String, required: true },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+const StudentModel = mongoose.model<StudentDocument>("Student", studentSchema);
+
+export default StudentModel;
