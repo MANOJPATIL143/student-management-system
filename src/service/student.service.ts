26d2d910cf6023e14fbb5bb60316ed930832834e
elijah.ndungu <elijahndungu30@gmail.com>
2023-07-15 18:37:35 +0300

    refactor: removing metrics for monitoring database

--- src/service/student.service.ts
+++ src/service/student.service.ts
@@ -3,20 +3,12 @@ import StudentModel, {
   StudentDocument,
   StudentInput,
 } from "../models/student.model";
-import { databaseResponseTimeHistogram } from "../utils/metrics";
 
 export async function createStudent(input: StudentInput) {
-  const metricsLabels = {
-    operation: "createStudent",
-  };
-
-  const timer = databaseResponseTimeHistogram.startTimer();
   try {
     const result = await StudentModel.create(input);
-    timer({ ...metricsLabels, success: "true" });
     return result;
   } catch (e) {
-    timer({ ...metricsLabels, success: "false" });
     throw e;
   }
 }
@@ -25,18 +17,12 @@ export async function findStudent(
   query: FilterQuery<StudentDocument>,
   options: QueryOptions = { lean: true }
 ) {
-  const metricsLabels = {
-    operation: "findStudent",
-  };
 
-  const timer = databaseResponseTimeHistogram.startTimer();
+
   try {
     const result = await StudentModel.findOne(query, {}, options);
-    timer({ ...metricsLabels, success: "true" });
     return result;
   } catch (e) {
-    timer({ ...metricsLabels, success: "false" });
-
     throw e;
   }
 }
